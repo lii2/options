42eff0ae075db31b185e8470142ed205e5716fbf
Chiyoung J Lii <lii.jeffrey3@gmail.com>
2019-01-02 18:06:05 -0700

    Fixed defect

--- src/main/java/com/options/entities/EntityData.java
+++ src/main/java/com/options/entities/EntityData.java
@@ -1,7 +1,12 @@
 package com.options.entities;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 abstract class EntityData {
-    transient static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("uuuu-MM-dd");
+    private transient static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("uuuu-MM-dd");
+
+    protected LocalDate parseDate(String dateString) {
+        return LocalDate.parse(dateString.substring(0, 10), DATE_TIME_FORMATTER);
+    }
 }
