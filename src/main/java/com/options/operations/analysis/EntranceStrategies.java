e3eee4e16dc9d5120278552ef0161d31a22e99cb
Chiyoung J Lii <lii.jeffrey3@gmail.com>
2019-01-15 04:53:25 -0500

    Fixed typo

--- src/main/java/com/options/operations/analysis/EntranceStrategies.java
+++ src/main/java/com/options/operations/analysis/EntranceStrategies.java
@@ -35,19 +35,19 @@ public class EntranceStrategies {
 
     private String generateRiseMessage(DailyData dailyData) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("\nprice rose above the ema, price was: ").append(dailyData.getPreviousDaysData().getOpenCloseMean())
-                .append("\nema is: ").append(dailyData.getEma())
-                .append("\nprice is now: ").append(dailyData.getOpenCloseMean())
-                .append("\nMessage: buy calls to sell.");
+        stringBuilder.append("\n price rose above the ema, price was: ").append(dailyData.getPreviousDaysData().getOpenCloseMean())
+                .append("\n ema is: ").append(dailyData.getEma())
+                .append("\n price is now: ").append(dailyData.getOpenCloseMean())
+                .append("\n Message: buy calls to sell.");
         return stringBuilder.toString();
     }
 
     private String generateDropMessage(DailyData dailyData) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("\nprice dropped below the ema, price was: ").append(dailyData.getPreviousDaysData().getOpenCloseMean())
-                .append("\nema is: ").append(dailyData.getEma())
-                .append("\nprice is now: ").append(dailyData.getOpenCloseMean())
-                .append("\nMessage: buy puts to sell.");
+        stringBuilder.append("\n price dropped below the ema, price was: ").append(dailyData.getPreviousDaysData().getOpenCloseMean())
+                .append("\n ema is: ").append(dailyData.getEma())
+                .append("\n price is now: ").append(dailyData.getOpenCloseMean())
+                .append("\n Message: buy puts to sell.");
         return stringBuilder.toString();
     }
 
