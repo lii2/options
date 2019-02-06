ef0d215c59a8c74026920eda5123ada0600aaa90
Chiyoung J Lii <lii.jeffrey3@gmail.com>
2019-02-06 12:04:24 -0500

    Fixed typoe

--- src/main/java/com/options/repositories/EmaRepository.java
+++ src/main/java/com/options/repositories/EmaRepository.java
@@ -1,8 +1,9 @@
 package com.options.repositories;
 
+import com.options.entities.EmaEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmaRepository extends CrudRepository<EmaRepository, Integer> {
+public interface EmaRepository extends CrudRepository<EmaEntity, Integer> {
 }
