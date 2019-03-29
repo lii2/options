63ecc09751162f42989b961d2a40b6108ad667a4
Chiyoung J Lii <lii.jeffrey3@gmail.com>
2019-03-29 14:28:54 -0400

    Added messages to ema crossing strategies

--- src/main/java/com/options/strategy/BearishEmaCrossingStrategy.java
+++ src/main/java/com/options/strategy/BearishEmaCrossingStrategy.java
@@ -7,8 +7,19 @@ import java.util.Optional;
 
 public class BearishEmaCrossingStrategy extends Strategy {
 
+    public final int VERSION = 1;
+
     @Override
     Optional<Recommendation> decide(DailyData dailyData) {
         return Optional.empty();
+    }
+
+    private String generateDropMessage(DailyData dailyData) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(" price dropped below the ema, price was: ").append(dailyData.getPreviousDaysData().getOpenCloseMean())
+                .append(" ema is: ").append(dailyData.getEma())
+                .append(" price is now: ").append(dailyData.getOpenCloseMean())
+                .append(" Message: buy puts to sell.");
+        return stringBuilder.toString();
     }
 }
--- src/main/java/com/options/strategy/BullishEmaCrossingStrategy.java
+++ src/main/java/com/options/strategy/BullishEmaCrossingStrategy.java
@@ -7,9 +7,20 @@ import java.util.Optional;
 
 public class BullishEmaCrossingStrategy extends Strategy {
 
+    public final int VERSION = 1;
+
     @Override
     Optional<Recommendation> decide(DailyData dailyData) {
         return Optional.empty();
+    }
+
+    private String generateRiseMessage(DailyData dailyData) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(" price rose above the ema, price was: ").append(dailyData.getPreviousDaysData().getOpenCloseMean())
+                .append(" ema is: ").append(dailyData.getEma() )
+                .append(" price is now: ").append(dailyData.getOpenCloseMean() )
+                .append(" Message: buy calls to sell.");
+        return stringBuilder.toString();
     }
 
 }
